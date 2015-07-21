ccca6c4908521f77ae46adeb81b4bcef7a1690f3
Thomas Nagler <thomas.nagler@tum.de>
2015-07-21 07:22:13 +0000

    version 0.0.3

--- src/renorm.cpp
+++ src/renorm.cpp
@@ -16,7 +16,7 @@ NumericVector ren_subs(NumericVector vals, NumericVector grid, int d) {
     
     // recursively integrate over a sequence of m values
     for (int j = 0; j < d; ++j){
-        for (int p = 0; p < pow(m, d - j - 1); ++p) {
+        for (int p = 0; p < pow((double)m, d - j - 1); ++p) {
             tmpvals = newvals[p*m + gridsq];
             newvals[p] = int_on_grid(1.0, tmpvals, grid);
         }
@@ -61,19 +61,38 @@ NumericVector renorm(NumericVector x, NumericVector grid, int times, IntegerMatr
     return M;
 }
 
-
-//NumericMatrix renorm_2d(NumericMatrix x, NumericVector grid, int times) {
-//    double m = x.nrow();
-//    const NumericMatrix xx(x);
-//    NumericMatrix M = clone(xx);
-//    
-//    for(int k = 0; k < times; ++k) {
-//        for (int i = 0; i < m; ++i) 
-//        M(i, _) = M(i, _)/int_on_grid(1.0, M(i, _), grid);
-//        for (int j = 0; j < m; ++j)
-//        M(_, j) = M(_, j)/int_on_grid(1.0, M(_, j), grid);
-//    }
-//    
-//    return M;
-//}
+//// calculate errors for uniform margins
+NumericMatrix calc_errors(NumericVector x, NumericVector grid, int times, IntegerMatrix helpind) {
+    double m = grid.size();
+    IntegerVector dims = x.attr("dim");
+    int d = dims.size();
+    int mtd = pow(m, d-1);
+    NumericMatrix err(times, m);
+    
+    NumericVector M(x.size());
+    M = clone(x);
+    IntegerMatrix tmpind(mtd, d);
+    
+    for(int t = 0; t < times; ++t) {
+        for (int j = 0; j < d; ++j) {
+            for (int k = 0; k < m; ++k) {
+                // get indices for subsetting array M
+                int pl = 0;
+                for (int jj = 0; jj < d; ++jj) {
+                    if (jj == j) {
+                        tmpind(_, jj) = rep(k, mtd);
+                    } else { 
+                        tmpind(_, jj) = helpind(_, pl);
+                        ++pl; 
+                    }
+                }
+                if (j == 0) {
+                    err(t, k) = int_on_grid(1.0, M[get(tmpind, dims)], grid);
+                }
+                M[get(tmpind, dims)] = ren_subs(M[get(tmpind, dims)], grid, d-1);
+            }
+        }
+    }
+    return err;
+}
 
